9d1fa310e952af448f3301b4aa0f44f57f5f7202
armenh <avarmenhg@gmail.com>
2017-04-12 13:38:55 -0400

    Initial commit

---
+++ src/server/server.js
@@ -0,0 +1,8 @@
+/**
+ * Created by aharutyunyan on 4/12/2017.
+ */
+import "source-map-support/register";
+import {blegh} from "../shared/blegh";
+
+blegh();
+console.log("FROM SERVER");
---
+++ src/shared/blegh.js
@@ -0,0 +1,6 @@
+/**
+ * Created by aharutyunyan on 4/12/2017.
+ */
+export function blegh() {
+	console.log("IT WORKED!");
+}
